19b7d474abc278e4be438515c5ffa0993453d0ea
Antonio Smecca <antoniosmecca@gmail.com>
2025-02-13 17:35:46 +0000

    minor fix

--- Gen2L/utils.py
+++ Gen2L/utils.py
@@ -54,11 +54,11 @@ def terms_corr_boot(path,channel,Nt,boot_samples):
 
 
 
-corr_g_zero_A, corr_g_conn_A, corr_cross_A, corr_disc_A, corr_sum_A, corr_total_A = [],[],[],[],[],[]
-corr_g_zero, corr_g_conn, corr_cross, corr_disc, corr_sum, corr_total = [],[],[],[],[],[]
-corr_t, T_lat = [],[]
 
 def get_correlators(path_to_corrs,boot_samples,Nt_array,T_array):
+    corr_g_zero_A, corr_g_conn_A, corr_cross_A, corr_disc_A, corr_sum_A, corr_total_A = [],[],[],[],[],[]
+    corr_g_zero, corr_g_conn, corr_cross, corr_disc, corr_sum, corr_total = [],[],[],[],[],[]
+    corr_t, T_lat = [],[]    
     for i in range(0,len(Nt_array)):
         Nt = Nt_array[i]
         T = T_array[i]
