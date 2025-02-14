b4c584b7761f2963e491a5740798b52806afa450
Antonio Smecca <antoniosmecca@gmail.com>
2025-02-14 17:26:07 +0000

    fixed path

--- gen2/iter_mu2.sh
+++ gen2/iter_mu2.sh
@@ -6,12 +6,12 @@ declare -a list_T=("0.141" "0.156" "0.176" "0.201" "0.235" "0.281" "0.352")
 declare -a list_mu=("0.028" "0.056" "0.084" "0.113" "0.14" "0.169" "0.197" "0.225" "0.281")
 
 
-for j in `seq 0 5`
+for j in `seq 0 8`
 do    
     for i in `seq 0 6`
     do	
 	Nt=${list_Nt[$i]}
-	path_to_correlator_directory=/Users/antoniosmecca/Documents/Physics/pdoc_Swansea/mu2/Code/DATA/Gen2L/${Nt}x24 	
+	path_to_correlator_directory=/DATA/gen2/${Nt}x24 	
 	T=${list_T[$i]}
 	mu=${list_mu[$j]}
 	echo $Nt
