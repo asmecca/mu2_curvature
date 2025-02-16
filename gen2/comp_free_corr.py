9deb6ec5283f81a107a53d2c0ffcc9056c34dd74
Antonio Smecca <antoniosmecca@gmail.com>
2025-02-16 16:52:23 +0100

    added T_lat

--- gen2/comp_free_corr.py
+++ gen2/comp_free_corr.py
@@ -46,6 +46,7 @@ marker_list=['v','<','^','>','o','s','p']
 color_list=['darkred','red','orange','gold','#71daeb','cyan','blue']
 a_inv_gev = 5.63
 T_array = [0.352, 0.281, 0.235, 0.201, 0.176, 0.156, 0.141]
+T_lat = np.asarray(T_array)/a_inv_gev
 tminarray = [3, 4, 4, 5, 6, 7, 8]#, 8]
 Nt_array =[16, 20, 24, 28, 32, 36, 40]
 
